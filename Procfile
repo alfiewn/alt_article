a20cc93a6ad27a562200837cd4afa706767f5329
alfiewn <alfiewn@gmail.com>
2021-04-13 13:55:49 +0100

    Pre-download CSV

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
+gdown https://drive.google.com/uc?id=1--bWl8eSVU0WPD-MH4E_elOaP8KU_NeJ
 web: gunicorn main:app
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app.config['JSON_SORT_KEYS'] = False
 
 @app.before_first_request
 def load_articles():
-    subprocess.run(["gdown", "--id", "1--bWl8eSVU0WPD-MH4E_elOaP8KU_NeJ"])
+    # subprocess.run(["gdown", "--id", "1--bWl8eSVU0WPD-MH4E_elOaP8KU_NeJ"])
     chunksize = 10000
     tfr = pd.read_csv('./articles_w_sentiment.csv', delimiter='\t', chunksize=chunksize, iterator=True)
     valid_article_df = pd.concat(tfr, ignore_index=True)
