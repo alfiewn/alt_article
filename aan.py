c338118938d67c43f9a6703e00c7bdabf7675a69
alfiewn <alfiewn@gmail.com>
2021-04-13 16:04:32 +0100

    Update functions

--- aan.py
+++ aan.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import numpy as np
 
+from itertools import starmap
+
 from collections import Counter
 
 from sklearn.metrics.pairwise import cosine_similarity
@@ -13,46 +15,36 @@ def in_memory(df):
   valid_article_df = df
 
 def contains_topic(other_topics, input_article_topic):
-  other_topics = eval(other_topics)
   for topic in other_topics:
     if topic in input_article_topic:
       return True
   return False
 
-def entity_string(entities, n):
+def entity_string(entities, n=10):
   top_entities = Counter(entities).most_common(n)
-  entityString = ""
+  entity_string = ""
   for entityTuple in top_entities:
     repeats = entityTuple[1]
     entity = entityTuple[0]
     for i in range(repeats):
-      entityString = entityString + " " + entity
-  return entityString
+      entity_string = entity_string + " " + entity
+  return entity_string.lstrip()
 
-def find_similarities(entities, input_article_entities):
-  similarities = []
-  for entity_list in entities:
-    to_compare = entity_string(eval(entity_list), 10)
+def find_similarity(input_entities, alternative_entities):
 
-    vector = CountVectorizer().fit_transform([input_article_entities, to_compare])
+  input_entities_string = entity_string(input_entities)
+  alternative_entites_string = entity_string(alternative_entities)
 
-    score = cosine_similarity(vector[0:1], vector).flatten()[1]
-    
-    similarities.append(score)
+  vector = CountVectorizer().fit_transform([input_entities_string, alternative_entites_string])
+  return cosine_similarity(vector[0:1], vector).flatten()[1]
 
-  return similarities
+def find_balance(input_article_sentiment, alternative_sentiment):
+  return round((alternative_sentiment + input_article_sentiment)/2, 3)
 
-def get_sentiment_balances(sentiments, input_article_sentiment):
-  sentiment_balances = []
-  for sentiment in sentiments:
-    sentiment_balance = abs(eval(sentiment)[0] + input_article_sentiment)
-    sentiment_balances.append(sentiment_balance)
-  return sentiment_balances
+def find_ranking(similarity_ranking, sentiment_ranking):
+  return (similarity_ranking + sentiment_ranking)
 
 def get_alternative_article(input_article_index):
-    
-  # valid_article_df = pd.read_csv('./articles_w_sentiment.csv', delimiter='\t')
-
   input_article_body = valid_article_df['body'][input_article_index]
   input_article_url = valid_article_df['url'][input_article_index]
 
@@ -61,40 +53,37 @@ def get_alternative_article(input_article_index):
   input_article_sentiment = eval(valid_article_df['sentiment'][input_article_index])[0]
 
 
-  matching_article_df = valid_article_df.loc[valid_article_df['topic'].apply(lambda topics: contains_topic(topics, input_article_topic))]
+  matching_article_df = valid_article_df.loc[valid_article_df['topic'].apply(lambda alternative_topic: contains_topic(eval(alternative_topic), input_article_topic))]
 
   matching_article_df = matching_article_df.drop([input_article_index])
 
   matching_entities = matching_article_df['entities']
-  matching_article_df['similarity'] = find_similarities(matching_entities, entity_string(input_article_entities, 10))
+  matching_article_df['similarity'] = matching_entities.map(lambda alternative_entities: find_similarity(eval(alternative_entities), input_article_entities))
 
   matching_sentiments = matching_article_df['sentiment']
-  matching_article_df['sentiment_balance'] = get_sentiment_balances(matching_sentiments, input_article_sentiment)
+  matching_article_df['sentiment_balance'] = matching_sentiments.map(lambda alternative_sentiment: find_balance(eval(alternative_sentiment)[0], input_article_sentiment))
 
   rankings = list(range(0, len(matching_article_df)))
 
   by_similarity = matching_article_df.sort_values(
       by='similarity', 
-      ascending=False
+      ascending=False,
+      key=abs
       )
 
   by_similarity['similarity_rank'] = rankings
 
   by_sentiment_balance = by_similarity.sort_values(
       by='sentiment_balance', 
-      ascending=True
+      ascending=True,
+      key=abs
       )
 
   by_sentiment_balance['balance_rank'] = rankings
 
   similarity_rankings = by_sentiment_balance['similarity_rank']
 
-  optimised_rankings = []
-  for similarity, sentiment in zip(similarity_rankings, rankings):
-      optimised_rankings.append(similarity + sentiment)
-
-  by_sentiment_balance['optimised'] = optimised_rankings
-
+  by_sentiment_balance['optimised'] = list(starmap(find_ranking, zip(similarity_rankings, rankings)))
 
   alternative_article = None if len(by_sentiment_balance) == 0 else by_sentiment_balance.sort_values(
       by='optimised', 
